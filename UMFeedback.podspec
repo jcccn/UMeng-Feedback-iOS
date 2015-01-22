002716df9406f3bbe56c7bf2cce60f142a90ed22
jcccn <jccuestc@gmail.com>
2015-01-22 13:26:35 +0800

    Update the Umeng Feedback v2.1 2015.01.04

--- UMFeedback.podspec
+++ UMFeedback.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name     = 'UMFeedback'
-  s.version  = '2.0'
+  s.version  = '2.1'
   s.summary  = 'Umeng Feedback is the leading mobile app feedback platform in China.'
   s.homepage = 'https://github.com/jcccn/UMeng-Feedback-iOS'
   s.author   = { 'Chuncheng Jiang' => 'jccuestc@gmail.com' }
@@ -8,11 +8,11 @@ Pod::Spec.new do |s|
                    © 2011-2015 Umeng.com , All Rights Reserved.
                  LICENSE
                }
-  s.source   = { :git => 'https://github.com/jcccn/UMeng-Feedback-iOS.git', :tag => '2.0' }
-  s.source_files = 'UMFeedback/UMFeedback.h'
+  s.source   = { :git => 'https://github.com/jcccn/UMeng-Feedback-iOS.git', :tag => '2.1' }
+  s.source_files = 'UMFeedback/*.{h,m}', 'UMFeedback/UMOpus/*.{h,m}'
   s.platform     = :ios, '5.0'
-  s.frameworks   = "UIKit"
-  s.vendored_libraries = 'UMFeedback/libUMFeedback.a'
-  s.resources = "UMFeedback/zh-Hans.lproj/UMFeedbackLocalizable.strings"
+  s.frameworks   = 'UIKit', 'AVFoundation'
+  s.vendored_libraries = 'UMFeedback/libUMFeedback.a', 'UMFeedback/UMOpus/libUMOpus.a'
+  s.resources = 'UMFeedback/zh-Hans.lproj/UMFeedbackLocalizable.strings', 'UMFeedback/Resources/*.{png}'
   s.requires_arc = true
 end
--- UMFeedback/UMFeedback.h
+++ UMFeedback/UMFeedback.h
@@ -5,11 +5,12 @@
 //  Created by ming hua on 2012-03-19.
 //  Updated by ming hua on 2013-04-17.
 //  Updated by cui guilin on 2014-09-12.
-//  Version 1.4
-//  Copyright (c) 2012年 umeng.com. All rights reserved.
+//  Version 2.1
+//  Copyright (c) 2014年 umeng.com. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "UMRecorder.h"
 #import <UIKit/UIKit.h>
 
 #define UMFBCheckFinishedNotification @"UMFBCheckFinishedNotification"
@@ -34,26 +35,27 @@
  */
 - (void)postFinishedWithError:(NSError *)error;
 
+- (void)stopRecordAndPlayback;
 @end
 
 
 #pragma mark - Feedback Object
-@interface UMFeedback : NSObject
+@interface UMFeedback : NSObject <RecorderDelegate>
 
 /**
  *  UMFeedback Data Delegate
  */
-@property(nonatomic, unsafe_unretained) NSObject <UMFeedbackDataDelegate> *delegate;
+@property (nonatomic, unsafe_unretained) NSObject <UMFeedbackDataDelegate> *delegate;
 
 /**
  *  the new replies
  */
-@property(nonatomic, strong) NSMutableArray *theNewReplies;
+@property (nonatomic, strong) NSMutableArray *theNewReplies;
 
 /**
- *  the topic and replies
+ *  the topic and replies, also contains the failed one
  */
-@property(nonatomic, strong) NSMutableArray *topicAndReplies;
+@property (nonatomic, strong) NSMutableArray *topicAndReplies;
 
 #pragma mark Settings
 
@@ -65,6 +67,7 @@
 + (void)setLogEnabled:(BOOL)value;
 
 + (BOOL)feedbackLogEnabled;
+- (BOOL)audioEnabled;
 
 /**
  *  set feedback app key. you can find out the app key at: http://www.umeng.com/apps/setting
@@ -73,7 +76,18 @@
  */
 + (void)setAppkey:(NSString *)appkey;
 
+/**
+ *  User Id
+ *
+ *  @return <#return value description#>
+ */
 + (NSString *)uuid;
+
+/**
+ *  message type for UMessage alias message type
+ *
+ *  @return <#return value description#>
+ */
 + (NSString *)messageType;
 
 #pragma mark Show Feedback View
@@ -104,16 +118,30 @@
  */
 + (UMFeedback *)sharedInstance;
 
+
+- (void)setBackButton:(UIButton *)button;
+
+// For iOS 7 and later
+/**
+ *  set Navigationbar title color and close button tint color
+ *
+ *  @param color <#color description#>
+ */
+- (void)setTitleColor:(UIColor *)color;
+
+
+// 数据接口
 /**
  *  get feedback replies from server
  */
 - (void)get;
 
 /**
- *  post feedback repli to server
+ *  post feedback reply to server
  *
  *  @param feedback_dictionary <#feedback_dictionary description#>
  */
+
 - (void)post:(NSDictionary *)feedback_dictionary;
 
 // set custom remark info
@@ -126,6 +154,14 @@
 
 - (void)updateUserInfo:(NSDictionary *)info;
 - (NSDictionary *)getUserInfo;
+
+// 推送相关
+- (void)setFeedbackViewController:(UIViewController *)controller shouldPush:(BOOL)shouldPush;
+
+// for record and playback
+- (void)stopRecordAndPlayback;
+- (void)playRecordWithReplyId:(NSString *)replyId;
+- (UMRecorder *)getRecorder;
 
 // 不建议使用的
 
